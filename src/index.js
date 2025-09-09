9953246349a68ccef6b45512af867d3bfac1d91f
Vitória Kaline <160519591+vitoriakaline@users.noreply.github.com>
2025-09-09 20:35:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+let nomeHeroi = "João";
+let XP = 2001;
+let nivel = "";
+
+if (XP <= 1000) { 
+  nivel = "Ferro";
+} else if (XP >= 1001 && XP <= 2000) { 
+  nivel = "Bronze";
+} else if (XP >= 2001 && XP <= 5000) { 
+  nivel = "Prata";
+} else if (XP >= 5001 && XP <= 7000) { 
+  nivel = "Ouro";
+} else if (XP >= 7001 && XP <= 8000) { 
+  nivel = "Platina";
+} else if (XP >= 8001 && XP <= 9000) { 
+  nivel = "Ascendente";
+} else if (XP >= 9001 && XP <= 10000) { 
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivel}`);
